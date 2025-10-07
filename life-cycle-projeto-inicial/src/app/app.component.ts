9f9c216be504df3e2a55fa90d37fed830007ad9f
Marcos Vogado <marcoshcvogado@gmail.com>
2025-10-07 13:49:00 -0300

    Render shopping list items with ItemComponent
    Updated app.component.html to display each item in the shopping list using the ItemComponent. Adjusted imports in app.component.ts to include ItemComponent and InputComponent. Fixed import path for Item interface in lista-de-compra.service.ts.

--- life-cycle-projeto-inicial/src/app/app.component.ts
+++ life-cycle-projeto-inicial/src/app/app.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { ListaDeCompraService } from './service/lista-de-compra.service';
 import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+import { ItemComponent } from "./components/item/item.component";
+import { InputComponent } from "./components/input/input.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [ItemComponent, InputComponent]
 })
 export class AppComponent implements OnInit {
   title = 'app-lista-de-compras';
-
   listaDeCompra! : Array<Item>
-
   constructor(private listaService: ListaDeCompraService) { }
-
+  
   ngOnInit(): void {
-    this.listaDeCompra = this.listaService.getListaDeCompra();
-    console.log(this.listaDeCompra)
+   this.listaDeCompra = this.listaService.getListaDeCompra();
+   console.log(this.listaDeCompra);
   }
 }
